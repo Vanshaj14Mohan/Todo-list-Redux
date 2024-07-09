6710ee95a987a836c1d9dcdaff4bc6baf8ddd4d5
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-09 23:07:01 +0530

    reducers

---
+++ redux-class/src/features/todo/todoSlice.js
@@ -0,0 +1,22 @@
+//will contain all the reducers:
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+
+const initialState = {
+    todos: [{id: "abc", task: "demo-task", isDone: false}],
+  };
+
+export const todoSlice = createSlice({
+    name: "todo",
+    initialState,
+    reducers:{ //state, action
+        addTodo: (state, action) =>{
+            const newTodo = {
+                id: nanoid(),
+                task: action.payload,
+                isDone: false,
+            }
+            state.todos.push(newTodo);
+        }
+
+    }
+})
