d30faa874e34710ede8cc355ad7993e9319c708d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-11 21:29:51 +0530

    basics

--- redux-class/src/features/todo/todoSlice.js
+++ redux-class/src/features/todo/todoSlice.js
@@ -16,6 +16,21 @@ export const todoSlice = createSlice({
                 isDone: false,
             };
             state.todos.push(newTodo); //direct mutation
-        }
-    }
-})
+        },
+        deleteTodo: (state, action) =>{
+            //action.payload
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+        },
+        marksAsDone: (state, action) =>{
+            //action.payload
+            state.todos = state.todos.map((todo) =>{
+                if(todo.id === action.payload) {
+                    todo.isDone = true;
+                }
+            });
+        },
+    },
+});
+
+export const { addTodo, deleteTodo, marksAsDone } = todoSlice.actions;
+export default todoSlice.reducer;
